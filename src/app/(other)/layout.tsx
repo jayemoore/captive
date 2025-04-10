7676e6a86ea6a9882484bc7eb8e229c1c68b1295
developer <developer@innovativemojo.com>
2025-04-10 18:16:19 +0500

    website is made live

--- src/app/(other)/layout.tsx
+++ src/app/(other)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+
 import AosProvider from "../providers/Aos";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -19,9 +18,7 @@ export default function OtherLayout({
 }>) {
   return (
     <AosProvider>
-      <Navbar />
       <main className={`${inter.variable}`}>{children}</main>
-      <Footer />
     </AosProvider>
   );
 }
